c20c513d129913b0640d1cfae5544295c0424ed5
ken <jitamm20081@gmail.com>
2025-06-07 23:22:24 +0800

    Initial Commit

---
+++ DemoAccumulation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoAccumulation
+//
+//  Created by ken on 2025/6/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
